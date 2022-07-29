326b77dd9aec374cc32ef606af9950aea76a9a49
VictorHugoFranz <victorcetelbras@hotmail.com>
2022-07-28 21:43:10 -0300

    atividade

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php 
+
+    $host    = 'localhost';
+    $user    = 'root';
+    $passwd  = 'victor1233';
+    $bd_name = 'atividade';
+ 
+    $connx = mymysqli_connect($host, $user, $passwd, $bd_name);
+
+
+?>
